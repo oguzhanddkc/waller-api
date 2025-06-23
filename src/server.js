3a65dd761af8b6cc78c980e4e1a3548ce89907dd
Oğuzhan Düdükcü <oguzhanddkc@gmail.com>
2025-06-23 15:23:59 +0300

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRouter from "./routes/transactionsRouter.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  job.start();
+}
+
 const PORT = process.env.PORT || 5001;
 
 // Middleware to parse JSON requests
@@ -15,6 +21,10 @@ app.use(express.json());
 //   console.log(`${req.method} request for '${req.url}'`);
 //   next();
 // });
+
+app.get("api/health", (req, res) => {
+  res.status(200).json({ message: "API is healthy" });
+});
 
 app.use("/api/transactions", transactionsRouter);
 initDB().then(() => {
